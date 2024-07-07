71dc14b84318d9b7c45e33b2c1597088d66dc06e
nklubega <nklubega.com>
2024-07-07 13:12:10 +0300

    added cards component

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -6,7 +6,18 @@ const HomeCards = () => {
     <section className="py-4">
     <div className="container-xl lg:container m-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-      <Card />
+        <Card>
+          <h2 className="text-2xl font-bold">For Developers</h2>
+          <p className="mt-2 mb-4">
+            Browse our React jobs and start your career today
+          </p>
+          <a
+            href="/jobs.html"
+            className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+          >
+            Browse Jobs
+          </a>
+        </Card>
         <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
           <h2 className="text-2xl font-bold">For Employers</h2>
           <p className="mt-2 mb-4">
